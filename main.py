ee723565df7d238d6350aeacea8c40affe3d2f2c
Kirill Semenov <kultur.smith@list.ru>
2016-05-16 00:01:18 +0500

    add draw mode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.title("Game Of Life")
+root.resizable(width=False, height=False)
 #canvas size in pixels
 HEIGHT = 500
 WIDTH = 600
@@ -9,28 +10,29 @@ CELL_SIZE = 5
 
 InGame = False
 
+#GUI
 top_frame = Frame(root)
 start_button = Button(top_frame, text="Start", width=10)
-stop_button = Button(top_frame, text="Stop", width=10)
+stop_button = Button(top_frame, text="Stop", width=10, state=DISABLED)
 clear_button = Button(top_frame, text="Clear", width=10)
 start_button.grid(row=0, column=0)
 stop_button.grid(row=0, column=1)
 clear_button.grid(row=0, column=2)
-top_frame.grid()
+top_frame.grid(pady=10)
 
 canvas = Canvas(root, height=HEIGHT, width=WIDTH, bg="white")
 canvas.grid()
+
 class Cell:
     def __init__(self, x, y):
         self.alive = False
         self.rect = canvas.create_rectangle(x, y, x + CELL_SIZE, y + CELL_SIZE, fill="white", outline="#dddddd")
-    def change_state(self):
-        if self.alive:
-            self.alive = False
-            self.rect["fill"] = "black"
-        else:
-            self.alive = True
-            self.rect["fill"] = "white"
+    def set_alive(self):
+        self.alive = True
+        canvas.itemconfig(self.rect, fill="black")
+    def set_dead(self):
+        self.alive = False
+        canvas.itemconfig(self.rect, fill="white")
 class Field:
     def __init__(self):
         self.matrix = []
@@ -38,6 +40,13 @@ class Field:
             temp = []
             for x in range(int(WIDTH/CELL_SIZE) - 1):
                 temp.append(Cell(x * CELL_SIZE + 5, y * CELL_SIZE + 5))
+            self.matrix.append(temp)
+    def draw_start_state(self, event):
+        if not InGame:
+            self.matrix[(event.y - 5)//CELL_SIZE][(event.x - 5)//CELL_SIZE].set_alive()
+
 
 field = Field()
+canvas.bind("<B1-Motion>", field.draw_start_state)
+canvas.bind("<ButtonPress-1>", field.draw_start_state)
 root.mainloop()
